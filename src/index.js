e607fea6e0191f34d088653613403e738bf8f619
Noe4kd <noelia4kd@gmail.com>
2022-08-12 10:08:32 +0100

    JS fixed (wrong API). Weather forecast working

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = `357f496043907e473ed70c8c25ecf66b`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
 }
